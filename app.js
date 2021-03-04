da36255186e6f80a91b438ddf0c1a507b937acd5
Utkarsh Kumar <utkarshkumar965@gmail.com>
2021-03-04 14:27:27 +0530

    Added  Search bar Function

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ app.get("/", function (req, res) {
             res.send("Error occured");
         else
             res.render("index", {
-                arr: obj
+                arr: obj,
+                index: 0
             });
     }).sort({
         total_problems_solved: -1
@@ -59,29 +60,58 @@ app.get("/", function (req, res) {
 app.get("/adduser", function (req, res) {
     res.render("adduser");
 });
+app.post("/", function (req, res) {
+    var data = req.body;
+    // console.log(data);
+    Student.find({
+        username: data.username
+    }, function (err, obj) {
+        if (err)
+            res.send("Error occured");
+        else
+            res.render("index", {
+                arr: obj,
+                index: 1
+            });
+    });
+});
 app.post('/adduser', function (req, res) {
     var mydata = req.body;
-    console.log(mydata);
-    const studentUrl = `https://competitive-coding-api.herokuapp.com/api/leetcode/${mydata.username}`;
-    (async () => {
-        const response = await request({
-            url: studentUrl,
-            headers: {
-                accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "accept-encoding": "gzip, deflate, br",
-                "accept-language": "en-GB,en-US;q=0.9,en;q=0.8"
-            },
-            gzip: true
-        });
-        var temp = JSON.parse(response);
-        temp["username"] = mydata.username;
-        console.log(typeof (temp));
-        var user = new Student(temp);
-        user.save().then(() => {
-            res.redirect("/");
-        });
-    })();
-
+    Student.find({
+        username: mydata.username
+    }, function (err, obj) {
+        if (err)
+            res.render("Incorrect1");
+        else {
+            if (obj.length >= 1) {
+                res.render("Already");
+            } else {
+                const studentUrl = `https://competitive-coding-api.herokuapp.com/api/leetcode/${mydata.username}`;
+                (async () => {
+                    const response = await request({
+                        url: studentUrl,
+                        headers: {
+                            accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                            "accept-encoding": "gzip, deflate, br",
+                            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8"
+                        },
+                        gzip: true
+                    });
+                    var temp = JSON.parse(response);
+                    if (temp.status == "Failed") {
+                        res.render("Incorrect");
+                    } else {
+                        temp["username"] = mydata.username;
+                        console.log(typeof (temp));
+                        var user = new Student(temp);
+                        user.save().then(() => {
+                            res.redirect("/");
+                        });
+                    }
+                })();
+            }
+        }
+    });
 });
 app.listen(port, () => {
     console.log(`example app is listening at http://localhost:${port}`);
